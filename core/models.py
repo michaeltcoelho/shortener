ce4407fb06a9e0f066bf221df5adc53e0e7d956c
Michael Coelho <michael.tcoelho@gmail.com>
2014-11-30 11:01:30 -0200

    user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+class User(AbstractBaseUser):
+    name  = models.CharField(_('Name'), max_length=150, required=True)
+    email = models.EmailField(_('Email'), max_length=255, required=True)
+
+    USERNAME_FIELDS = 'email'
+
+    def __unicode__(self):
+        return '<%s:%s>' % (self.name, self.email, )
+
+    def get_full_name(self):
+        return '%s' % self.name
+
+    def get_short_name(self):
+        return '%s' % self.name
+
+
+
+
+
+
+
